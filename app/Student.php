31803d07aee7df3c49739a3d5f2bc8218cba6080
tareq12345 <khaledtareq44@gmail.com>
2020-03-18 20:24:05 +0200

    Update Student.php
    add relation with user

--- app/Student.php
+++ app/Student.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    public function courses()
+    {
+      return $this->belongsToMany('App\Course')->withTimestamps();
+    }
+
+    public function user() {
+      return $this->morphOne('App\User', 'userable');
+  }
 }
