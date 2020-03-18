84f3c72ba637dea53fc9e1a9bc9c89e8ca9037a6
tareq12345 <khaledtareq44@gmail.com>
2020-03-18 17:23:32 +0200

    Update _form.blade.php

--- resources/views/users/_form.blade.php
+++ resources/views/users/_form.blade.php
@@ -20,17 +20,42 @@
     </div>
 </div>
 
-<div class="form-group row">
+@if (Auth::user()->role_id == 2 || $user->role_id == 2)
+    <div class="form-group{{ $errors->has('qualification') ? ' has-error' : '' }}">
+        {!! Form::label('qualification', 'Qualification', ['class' => 'col-sm-3 control-label']) !!}
+        <div class="col-sm-3">
+            {!! Form::text('qualification', $user->userable->qualification, ['class' => 'form-control']) !!}
+            <span class="help-block text-danger">
+                {{ $errors -> first('qualification') }}
+            </span>
+        </div>
+    </div>
+@endif
+
+@if (Auth::user()->role_id == 1 && $user->role->id == 1)
+    <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+        {!! Form::label('phone', 'Phone', ['class' => 'col-sm-3 control-label']) !!}
+        <div class="col-sm-3">
+            {!! Form::text('phone', Auth::user()->userable->phone, ['class' => 'form-control']) !!}
+            <span class="help-block text-danger">
+                {{ $errors -> first('phone') }}
+            </span>
+        </div>
+    </div>
+@endif
+
+<div class="form-group row col-sm-6">
     <label class="" for="cover_image">Select a file:</label>
     <input type="file" id="cover_image" name="cover_image">                           
 </div>
 
-<div class="form-group row">
+<div class="mb-2">Select gender:</div>
+<div class="form-group row col-sm-6">
     <label class="" for="gender">male:</label>
     <input type="radio" id="gender" name="gender" value="male">                           
 </div>
 
-<div class="form-group row">
+<div class="form-group row col-sm-6">
     <label class="" for="gender">female:</label>
     <input type="radio" id="gender" name="gender" value="female">                           
 </div>
