6ab9ab417c58ed8dce14cd2d221de7bfe3def7dd
tareq12345 <khaledtareq44@gmail.com>
2020-03-18 16:50:25 +0200

    Update Course.php
    add relationship between models

--- app/Course.php
+++ app/Course.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    public function students()
+  {
+    return $this->belongsToMany('App\Student')->withTimestamps();
+  }
+
+  public function professors()
+  {
+    return $this->belongsToMany('App\Professor')->withTimestamps();
+  }
+
+  public function lectures(){
+    return $this->hasMany('App\Lecture');
+  }
+
+  public function department(){
+    return $this->belongsTo('App\Department');
+  }
+
+  public function level(){
+    return $this->belongsTo('App\Level');
+}
 }
