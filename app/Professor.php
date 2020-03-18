776cadc88e47d94ad34e3b4043181df9f2160369
tareq12345 <khaledtareq44@gmail.com>
2020-03-18 19:56:25 +0200

    Update Professor.php
    add relation between user and professor

--- app/Professor.php
+++ app/Professor.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Professor extends Model
 {
-    //
+    public function courses()
+    {
+      return $this->belongsToMany('App\Course')->withTimestamps();
+    }
+
+    public function user() {
+        return $this->morphOne('App\User', 'userable');
+    }
 }
