362f43a11e2dcb5d655eaa0f23ce9bf0a13a3100
unknown <khaledtareq44@gmail.com>
2020-07-22 00:04:54 +0200

    remove Refister API

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -31,34 +31,7 @@ public $successStatus = 200;
             return response()->json([$success], $this-> successStatus);
         } 
     }
-/** 
-     * Register api 
-     * 
-     * @return \Illuminate\Http\Response 
-     */ 
-    public function register(Request $request) 
-    { 
-        $validator = Validator::make($request->all(), [ 
-            'name' => 'required', 
-            'email' => 'required|email', 
-            'password' => 'required', 
-            'c_password' => 'required|same:password', 
-        ]);
-if ($validator->fails()) { 
-            return response()->json(['error'=>$validator->errors()], 401);            
-        }
-$input = $request->all(); 
-        $input['password'] = bcrypt($input['password']); 
-        $user = User::create($input); 
-        $success['token'] =  $user->createToken('MyApp')-> accessToken; 
-        $success['name'] =  $user->name;
-return response()->json(['success'=>$success], $this-> successStatus); 
-    }
-/** 
-     * details api 
-     * 
-     * @return \Illuminate\Http\Response 
-     */ 
+
     public function details() 
     { 
         $user = Auth::user(); 
